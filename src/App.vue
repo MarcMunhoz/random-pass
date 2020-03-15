7025572708f9c89d7993467adda1887686d1425b
Marcelo Munhoz <me@marcelomunhoz.com>
2020-03-15 17:38:34 -0300

    Layout

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,37 @@
   <div id="app">
     <h1>Texto randômico</h1>
     <cite>Texto composto por maiúsculas, minúsculas, números e carateres especiais.</cite>
-    <hr noshade>
-    <p>{{ theString }}</p>
-    <b-button
-      type="button"
-      class="btn-copy"
-      variant="secondary"
-      v-clipboard:copy="theString"
-      @click="changeText()"
-    >Copiar</b-button>
+    <b-card
+      class="w-75 mx-auto"
+      border-variant="danger"
+      header="O que são senhas"
+      header-bg-variant="danger"
+    >
+      <b-card-text>Esse emaranhado de letras, símbolos e números pode ser considerado o primeiro sistema de defesa ativo de um usuário de determinada plataformam. Com o avanço da tecnologia, faz-se necessário senhas mais difíceis de serem quebradas. Alugumas dicas:
+        <b-list-group>
+          <b-list-group-item variant="secondary">
+            Dê preferência a senhas sem referências ao mundo
+            <em>real</em>. Exemplo: casa-da-praia-em-maragogi
+          </b-list-group-item>
+          <b-list-group-item variant="secondary">Combine letras, números e símbolos</b-list-group-item>
+          <b-list-group-item
+            variant="secondary"
+          >Caso guarde as senhas digitalmente, use sempre criptografia</b-list-group-item>
+          <b-list-group-item variant="secondary">Troque as senhas regurlamente</b-list-group-item>
+          <b-list-group-item
+            variant="secondary"
+          >Compartilhe suas senhas apenas com pessoas de sua total confiança</b-list-group-item>
+        </b-list-group>
+      </b-card-text>
+      <p>{{ theString }}</p>
+      <b-button
+        type="button"
+        class="btn-copy"
+        variant="secondary"
+        v-clipboard:copy="theString"
+        @click="changeText()"
+      >Copiar</b-button>
+    </b-card>
   </div>
 </template>
 
@@ -70,8 +92,9 @@ export default {
   margin-top: 60px;
 }
 
-hr {
-  border: 1px solid hsl(0, 0%, 83%) !important;
+cite {
+  display: block;
+  margin-bottom: 1em;
 }
 
 .btn-copy {
