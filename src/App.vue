9c9e8d0ea3699030ca4e38de71f389a4be510160
Marcelo Munhoz <me@marcelomunhoz.com>
2020-10-26 13:52:18 -0300

    New feature: the password length can be modified Unused files was deleted.

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,63 @@
 <template>
   <div id="app">
     <h1>Texto randômico</h1>
-    <cite>Texto composto por maiúsculas, minúsculas, números e carateres especiais.</cite>
+    <cite
+      >Texto composto por maiúsculas, minúsculas, números e carateres
+      especiais.</cite
+    >
     <b-card
       class="w-75 mx-auto"
       border-variant="danger"
       header="O QUE SÃO SENHAS"
       header-bg-variant="success"
     >
-      <b-card-text>Esse emaranhado de letras, símbolos e números pode ser considerado o primeiro sistema de defesa ativo de um usuário de determinada plataformam. Com o avanço da tecnologia, faz-se necessário senhas mais difíceis de serem quebradas. Algumas dicas:
+      <b-card-text
+        >Esse emaranhado de letras, símbolos e números pode ser considerado o
+        primeiro sistema de defesa ativo de um usuário de determinada
+        plataforma. Com o avanço da tecnologia, faz-se necessário senhas mais
+        difíceis de serem quebradas. Algumas dicas:
         <b-list-group class="mt-2">
           <b-list-group-item variant="secondary">
             Dê preferência a senhas sem referências ao mundo
-            <em>real</em>. Exemplo: casa-da-praia-em-maragogi
+            <em>real</em>. Exemplo do que <strong>não</strong> fazer:
+            <del>casa-da-praia-em-maragogi</del>
           </b-list-group-item>
-          <b-list-group-item variant="secondary">Combine letras, números e símbolos</b-list-group-item>
-          <b-list-group-item
-            variant="secondary"
-          >Caso guarde as senhas digitalmente, use sempre criptografia</b-list-group-item>
-          <b-list-group-item variant="secondary">Troque as senhas regurlamente</b-list-group-item>
-          <b-list-group-item
-            variant="secondary"
-          >Compartilhe suas senhas apenas com pessoas de sua total confiança</b-list-group-item>
+          <b-list-group-item variant="secondary"
+            >Combine letras, números e símbolos</b-list-group-item
+          >
+          <b-list-group-item variant="secondary"
+            >Caso guarde as senhas digitalmente, use sempre
+            criptografia</b-list-group-item
+          >
+          <b-list-group-item variant="secondary"
+            >Troque as senhas regularmente</b-list-group-item
+          >
+          <b-list-group-item variant="secondary"
+            >Compartilhe suas senhas apenas com pessoas de sua total
+            confiança</b-list-group-item
+          >
         </b-list-group>
       </b-card-text>
       <p>
         Para ajuda um pouco, abaixo serão exibidas
-        <em>strings</em> geradas de forma randômica, com 16 caracteres. (entre os olhos). Cada 5 segundos uma nova
-        <em>string</em> é gerada.
+        <em>strings</em> geradas de forma randômica. (entre os olhos). Cada 5
+        segundos uma nova <em>string</em> é gerada.
+      </p>
+
+      <p>
+        <label for="passLen">Quantos carateres terá sua senha?</label>
+        <input
+          type="number"
+          name="passLen"
+          min="10"
+          max="99"
+          v-model="stringLen"
+        />
       </p>
       <p class="font-weight-bolder shadow-sm">
-        <b-icon icon="eye-slash-fill"/>
+        <b-icon icon="eye-slash-fill" />
         {{ theString }}
-        <b-icon icon="eye-slash"/>
+        <b-icon icon="eye-slash" />
       </p>
       <b-button
         type="button"
@@ -40,7 +65,8 @@
         variant="secondary"
         v-clipboard:copy="theString"
         @click="changeText()"
-      >Copiar</b-button>
+        >Copiar</b-button
+      >
     </b-card>
   </div>
 </template>
@@ -53,7 +79,8 @@ export default {
   name: "App",
   data() {
     return {
-      theString: "Seu texto apareçará aqui"
+      theString: "Seu texto apareçará aqui",
+      stringLen: "10",
     };
   },
   methods: {
@@ -64,6 +91,7 @@ export default {
 
       for (let i = 0; i < len; i++) {
         text += chars.charAt(Math.floor(Math.random() * chars.length));
+        //text = [...new Set(text)];
       }
 
       return text;
@@ -80,14 +108,14 @@ export default {
         btn[0].classList.remove("success");
         btn[0].innerHTML = "Copiar";
       }, 1500);
-    }
+    },
   },
   mounted() {
     setInterval(() => {
-      let theString = this.rndStr(16);
+      let theString = this.rndStr(this.stringLen);
       this.theString = theString;
     }, 5000);
-  }
+  },
 };
 </script>
 
@@ -99,6 +127,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+label {
+  display: block;
+  margin: 0 auto;
+}
+
+input {
+  text-align: center;
 }
 
 cite {
